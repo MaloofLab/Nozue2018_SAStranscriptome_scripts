ce98fd6cca381b2870b848cad6043d958802c420
Kazunari Nozue <knozue@ucdavis.edu>
2016-11-07 17:43:54 -0800

    110716 new project start. new function

--- SAS_muts_time_course_RNAseq_finalfinal/function_RNAseq_time_course.R
+++ SAS_muts_time_course_RNAseq_finalfinal/function_RNAseq_time_course.R
@@ -7,7 +7,8 @@
 library(edgeR);library(ggplot2);library(reshape2);library(grid);library(class);library(MASS);library(plyr)
 # library(kohonen) # for SOM analysis
 library(scales) # for muted
-library(WGCNA);library(ShortRead);library(goseq);library(GO.db); library("org.At.tair.db");library("annotate")
+library(WGCNA);library(ShortRead);library(goseq);library(GO.db); library("org.At.tair.db")
+#library("annotate") # does not work in whitney
 # see http://www.bioconductor.org/install/ for installation of these packages 
 #library(ggdendro) # for dendrogram
 library(lmerTest) # for significant analysis
